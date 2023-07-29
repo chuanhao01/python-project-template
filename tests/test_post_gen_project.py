7aa3136f8707fc868cf04016ffc5e25c7669bcac
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-07-29 18:06:34 +0800

    [feat] adding test for post_gen generate license function

--- tests/test_post_gen_project.py
+++ tests/test_post_gen_project.py
@@ -3,12 +3,13 @@ from pathlib import Path
 from hooks.post_gen_project import generate_license
 
 
-def test_generate_license(tmp_path: Path):
+def test_generate_license(tmp_path: Path) -> None:
     # Setting licence dir
-    (tmp_path / "_license").mkdir()
-    mit_licence_file = tmp_path / "_license" / "mit.txt"
-    with open(mit_licence_file, "w+") as f:
-        f.write("This is an example mit licence file")
+    (tmp_path / "_licenses").mkdir()
+    mit_licence_file_path = tmp_path / "_licenses" / "mit.txt"
+    with open(mit_licence_file_path, "w+", encoding="utf-8") as mit_license_file:
+        mit_license_file.write("This is an example mit licence file")
 
-    generate_license(tmp_path, "mit")
-    assert tmp_path / "LICENSE"
+    generate_license(tmp_path, "MIT")
+    assert (tmp_path / "LICENSE").exists()
+    assert not (tmp_path / "_license").exists()
