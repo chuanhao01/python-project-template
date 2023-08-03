3fe13dc4be3615811ce439414d2d7e5a6b714375
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-08-03 10:43:43 +0800

    [feat] adding generate pyproject post gen hook
    Description
    Generates the pyproject.toml file as well as adds dev packages and
    configs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 """This module is called after project is created."""
 
+import subprocess
 import textwrap
 from pathlib import Path
 from shutil import move, rmtree
@@ -9,8 +10,11 @@ PROJECT_PATH = Path.cwd().absolute()
 PROJECT_NAME = "{{ cookiecutter.project_name }}"
 PROJECT_MODULE = "{{ cookiecutter.project_name.replace('-', '_') }}"
 
-# Values to generate correct license
+PROJECT_DESCRIPTION = "{{ cookiecutter.project_description }}"
 LICENSE = "{{ cookiecutter.license }}"
+PYTHON_VERSION = "{{ cookiecutter.python_version }}"
+AUTHOR_NAME = "{{ cookiecutter.author_name }}"
+AUTHOR_EMAIL = "{{ cookiecutter.author_email }}"
 
 
 def generate_license(project_path: Path, licence: str) -> None:
@@ -32,13 +36,90 @@ def generate_license(project_path: Path, licence: str) -> None:
     )
 
 
-def generate_pyproject(project_path: Path)
+def generate_pyproject(
+    project_path: Path,
+    project_name: str,
+    project_description: str,
+    _license: str,
+    python_version: str,
+    author_name: str,
+    author_email: str,
+) -> None:
     """Cleans up and removes the _template folder
 
     Args:
         project_path: path to the project root directory
     """
+    # Create pyproject.toml
+    subprocess.run(
+        [
+            "poetry",
+            "init",
+            "--name",
+            project_name,
+            "--description",
+            project_description,
+            "--license",
+            _license,
+            "--author",
+            f"{author_name} <{author_email}>",
+            "--python",
+            f"^{python_version}",
+            "-n",
+        ],
+        check=True,
+    )
+    # Adding dev packages
+    subprocess.run(
+        ["poetry", "add", "-G", "dev", "--allow-prereleases", "black@latest", "-n"], check=True
+    )
+    subprocess.run(
+        [
+            "poetry",
+            "add",
+            "-G",
+            "dev",
+            "mypy@latest",
+            "mypy-extensions@latest",
+            "pylint@latest",
+            "isort[colors]@latest",
+            "pydocstyle@latest",
+            "coverage@latest",
+            "coverage-badge@latest",
+            "pytest@latest",
+            "pytest-html@latest",
+            "pytest-cov@latest",
+            "bandit@latest",
+            "pyupgrade@latest",
+            "pre-commit@latest",
+            "-n",
+        ],
+        check=True,
+    )
+    # Adding .additional/pyproject toml configs
+    with open(project_path / "pyproject.toml", "a", encoding="UTF-8") as pyproject_file:
+        pyproject_file.write("\n")
 
+        pyproject_template_path = project_path / "_template" / "pyproject"
+        with open(pyproject_template_path / "pylint.toml", encoding="UTF-8") as pylint_file:
+            pyproject_file.write(pylint_file.read())
+            pyproject_file.write("\n")
+
+        with open(pyproject_template_path / "black.toml", encoding="UTF-8") as black_file:
+            pyproject_file.write(black_file.read())
+            pyproject_file.write("\n")
+
+        with open(pyproject_template_path / "mypy.toml", encoding="UTF-8") as mypy_file:
+            pyproject_file.write(mypy_file.read())
+            pyproject_file.write("\n")
+
+        with open(pyproject_template_path / "pytest.toml", encoding="UTF-8") as pytest_file:
+            pyproject_file.write(pytest_file.read())
+            pyproject_file.write("\n")
+
+        with open(pyproject_template_path / "coverage.toml", encoding="UTF-8") as coverage_file:
+            pyproject_file.write(coverage_file.read())
+            pyproject_file.write("\n")
 
 
 def clean_up_template(project_path: Path) -> None:
@@ -82,6 +163,15 @@ def print_futher_instuctions(project_name: str) -> None:
 
 def main() -> None:
     generate_license(PROJECT_PATH, LICENSE)
+    generate_pyproject(
+        PROJECT_PATH,
+        PROJECT_NAME,
+        PROJECT_DESCRIPTION,
+        LICENSE,
+        PYTHON_VERSION,
+        AUTHOR_NAME,
+        AUTHOR_EMAIL,
+    )
     clean_up_template(PROJECT_PATH)
 
 
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,11 +6,19 @@ from cookiecutter.main import cookiecutter
 
 
 class TestCookiecutterGeneration:
-    def test_valid_project_name(self, tmp_path: Path) -> None:
+    def test_valid_project(self, tmp_path: Path) -> None:
         cookiecutter(
             template=Path.cwd().absolute().as_posix(),
             no_input=True,
-            extra_context={"project_name": "valid-project-name"},
+            extra_context={
+                "project_name": "valid-project-name",
+                "project_description": "This is a valid test project description",
+                "license": "BSD-3",
+                "python_version": "3.11",
+                "author_name": "wow username",
+                "author_email": "Wowemail@mail.com",
+                "line_length": 88,
+            },
             output_dir=tmp_path.absolute().as_posix(),
         )
 
@@ -22,10 +30,13 @@ class TestCookiecutterGeneration:
         # LICENSE file generated correctly
         assert (project_folder_path / "LICENSE").exists()
 
+        # Assert pyproject is generated correctly
+        assert (project_folder_path / "pyproject.toml").exists()
+
         # _template folder cleaned up
         assert not (project_folder_path / "_template").exists()
 
-    def test_invalid_project_name(self, tmp_path: Path, capsys: pytest.CaptureFixture) -> None:
+    def test_invalid_project(self, tmp_path: Path, capsys: pytest.CaptureFixture) -> None:
         with pytest.raises(FailedHookException):
             cookiecutter(
                 template=Path.cwd().absolute().as_posix(),
